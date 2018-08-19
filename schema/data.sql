0478b53c7e6cd8ff0e45e17c3dcc1d4f09f2bf42
Artur Karapetov <858683@gmail.com>
2018-08-20 00:27:01 +0300

    fix test data

--- schema/data.sql
+++ schema/data.sql
@@ -6,7 +6,7 @@ VALUES ('cf402144-0c02-4b97-98f2-73f7b56160cf', 't@t.t',
 
 -- ios and ikev2
 INSERT INTO public.user_device (uuid, user_uuid, device_token, device_id, virtual_ip, device_ip, platform_id, vpn_type_id, location, is_active, is_connected)
-VALUES ('78b752a5-2928-48a9-9fa7-ba8bebad9f61', 'cf402144-0c02-4b97-98f2-73f7b56160cf', '123', '321', '10.0.0.2', '192.168.0.12', 1, 2, 'Moscow', TRUE, TRUE);
+VALUES ('78b752a5-2928-48a9-9fa7-ba8bebad9f61', 'cf402144-0c02-4b97-98f2-73f7b56160cf', '123', '321', '10.0.0.2', '192.168.0.12', 1, 2, 'Moscow', TRUE, FALSE );
 -- windows and openvpn
 INSERT INTO public.user_device (uuid, user_uuid, device_token, device_id, virtual_ip, device_ip, platform_id, vpn_type_id, location, is_active, is_connected)
-VALUES ('4c23dffb-2cf2-4173-9d0c-e38caad6e12b', 'cf402144-0c02-4b97-98f2-73f7b56160cf', '456', '654', '10.0.0.1', '127.0.0.1', 3, 1, 'Los Angeles', TRUE, FALSE);
+VALUES ('4c23dffb-2cf2-4173-9d0c-e38caad6e12b', 'cf402144-0c02-4b97-98f2-73f7b56160cf', '456', '654', '10.0.1.10', '127.0.0.1', 3, 1, 'Los Angeles', TRUE, FALSE);
